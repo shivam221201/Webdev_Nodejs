e9cf86a4c1dc10f0ce873d18123469aa035e2efc
shivam221201 <500061432@stu.upes.ac.in>
2019-06-27 00:25:54 +0530

    These file contains js basic concepts.

---
+++ lecture3/Hoisting/script.js
@@ -0,0 +1,10 @@
+
+sayHello("Dhruv");
+let n = 5;
+
+function sayHello(name) {
+
+    for(var i=0; i<n; i++) {
+        console.log("Hello " + name)
+    }
+}
---
+++ lecture3/index.js
@@ -0,0 +1,121 @@
+var x = "Hello world"
+console.log("x")
+
+typeof 2
+
+typeof shivam
+
+console.log("hello world")
+hello world 
+
+var obj ={}
+
+var obj ={name: "abhishek", age: "21" }
+
+obj.name
+
+obj.age
+
+1+2
+
+true + false
+
+true +1
+
+true +"1"
+
+true == 1
+
+false == 0
+
+typeof nan
+
+
+2.0/"dqd"
+
+typeof NaN
+
+isNaN(2/"sas")
+
+isNaN(2/"3")
+
+typeof y
+
+y == u
+
+typeof y
+
+y == undefined
+
+typeof typeof 2
+
+1=== "1"
+
+shivam === "shivam"
+
+1 + "1" === 11
+
+1 + "1" === '11'
+
+isNaN(NaN)
+
+{} + []
+0
+obj.toString{}
+
+[] + {}
+
+[]
+
+{}
+
+"{}"
+
+
+{} + {}
+
+window.tostring{}
+
+5+{}
+
+{} + 5
+
+
+/*
+f1()
+   function f1 (){
+       var z =1
+    console.log(z)
+}
+
+
+
+f1()
+function f1 (){
+    var z =1
+    console.log(z)
+}
+
+
+var f1= function (){
+    var z =1;
+    console.log(z)
+}
+
+
+
+function f1 (i , onDone) {
+    var string = ""
+    for (var z=0; z <i ; z++)
+    string += z
+    
+    onDone(string)
+}
+
+f1(10, function(string){
+    window.alert(String)
+})
+
+*/
+
+
